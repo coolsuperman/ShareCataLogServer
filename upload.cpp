fc475a0982a6502d279cf5585f6d39af7abfa305
coolsuperman <846069147@qq.com>
2019-07-02 14:02:04 +0800

    上传功能模块

---
+++ upload.cpp
@@ -0,0 +1,10 @@
+#include"HttpRequest.hpp"
+
+int main(){
+    std::string rsp_body;
+    rsp_body="<html><body><h1>上传成功！</h1></body></html>";
+    std::cout<<rsp_body;
+    fflush(stdout);
+  return 0;
+}
+
